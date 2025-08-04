1461132803cee08e42bb8232c28ffa9d14e9d493
Adam Mwaniki <adamndegwa@protonmail.com>
2025-08-04 18:01:18 +0300

    retry deploy 10

--- 404.js
+++ 404.js
@@ -1,4 +1,15 @@
+import { getAssetFromKV } from '@cloudflare/kv-asset-handler';
 
-// dummy entry point for wrangler deploy (not used)
-export default {};
+addEventListener('fetch', event => {
+  event.respondWith(handleEvent(event));
+});
 
+async function handleEvent(event) {
+  try {
+    // Try to serve static assets from KV
+    return await getAssetFromKV(event);
+  } catch (e) {
+    // If asset not found, return a simple 404 response
+    return new Response('404 - Not Found', { status: 404 });
+  }
+}
